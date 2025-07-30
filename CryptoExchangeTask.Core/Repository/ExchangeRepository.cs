68cd90522b5e6600e248b86d072a433f655c6f32
Stephen Crouch <stephen.crouch@adesso.de>
2025-07-30 08:51:34 +0200

    Switch to reading exchange data from filesystem

--- CryptoExchangeTask.Core/Repository/ExchangeRepository.cs
+++ CryptoExchangeTask.Core/Repository/ExchangeRepository.cs
@@ -1,4 +1,5 @@
 ﻿using CryptoExchangeTask.Core.Model;
+using System.Reflection;
 using System.Text.Json;
 
 namespace CryptoExchangeTask.Core.Repository;
@@ -7,13 +8,12 @@ public class ExchangeRepository : IExchangeRepository
 {
     public ExchangeRepository()
     {
-        var assembly = GetType().Assembly;
-
-        Exchanges = assembly
-            .GetManifestResourceNames()
+        var folder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ??
+            throw new InvalidOperationException("Unable to find location of data files");
+        Exchanges = Directory.EnumerateFiles(Path.Combine(folder, "Data"))
             .SelectMany(rn =>
             {
-                using var stream = assembly.GetManifestResourceStream(rn);
+                using var stream = File.OpenRead(rn);
                 if (stream != null)
                 {
                     var exchange = JsonSerializer.Deserialize<Exchange>(stream);
